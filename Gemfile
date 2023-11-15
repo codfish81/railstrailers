87d654acc47f4867cb476c36d470a570bc062bfd
Cade Hansen <codfish81@byui.edu>
2023-11-15 13:04:35 -0700

    changed back

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem "rails", "~> 7.0.8"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
-
+gem 'mongoid', '~> 8.1', '>= 8.1.3'
 #gem 'pg', '~> 1.5', '>= 1.5.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.6', '>= 1.6.8'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative "boot"
 
 require "rails/all"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +14,7 @@ module Trailers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    #Mongoid.load! './config/mongoid.yml'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
